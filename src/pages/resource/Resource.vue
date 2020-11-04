46101466fb2319bc1bc90ef2a3710ba457e3590e
Martin <765936728@qq.com>
2020-11-04 23:18:48 +0800

    修改sdk加密逻辑为aes

--- src/pages/resource/Resource.vue
+++ src/pages/resource/Resource.vue
@@ -165,11 +165,22 @@
 					<a-form-model-item label="文件" prop="fileName">
 						<a-input v-model="addrForm.fileName" disabled/>
 					</a-form-model-item>
-					<a-form-model-item label="有效时间" prop="expireSeconds">
-						<a-input-number v-model="addrForm.expireSeconds" style="width: 100%"/>
+					<a-form-model-item label="有效时间" prop="expireNumber">
+						<a-input :min="1" :disabled="addrForm.isPublic || addrForm.expireUnit==='ever'"
+								 type="number" v-model="addrForm.expireNumber" style="width: 100%">
+							<a-select :disabled="addrForm.isPublic" slot="addonAfter" v-model="addrForm.expireUnit" style="width: 70px"
+									  @change="onExpireUnitChange">
+								<a-select-option value="minute">分钟</a-select-option>
+								<a-select-option value="hour">小时</a-select-option>
+								<a-select-option value="day">天</a-select-option>
+								<a-select-option value="month">月</a-select-option>
+								<a-select-option value="year">年</a-select-option>
+								<a-select-option value="ever">永久</a-select-option>
+							</a-select>
+						</a-input>
 					</a-form-model-item>
 					<a-form-model-item label="访问链" prop="signUrl">
-						<a-input v-model="addrForm.signUrl"/>
+						<a-input v-model="addrForm.signUrl" type="textarea"/>
 					</a-form-model-item>
 				</a-form-model>
 			</a-modal>
@@ -181,7 +192,7 @@
 						:wrapper-col="{span:12}"
 						:model="editForm">
 					<a-form-model-item label="资源名" prop="pathname">
-						<a-input v-model="editForm.pathname"/>
+						<a-input v-model="editForm.pathname" @pressEnter="editHandleOk"/>
 					</a-form-model-item>
 					<a-form-model-item label="权限" prop="isPublic">
 						<a-radio-group v-model="editForm.isPublic">
@@ -303,7 +314,9 @@
                     openId: null,
                     resourceId: null,
                     expireSeconds: 3600,
-                    bucketId: null
+                    bucketId: null,
+                    expireNumber: 2,
+                    expireUnit: 'hour',
                 },
                 openIds: [],
                 editVisible: false,
@@ -404,6 +417,11 @@
 
         },
         watch: {
+            "addrForm.expireNumber"(){
+                if(this.addrForm.expireNumber < 1){
+              		this.addrForm.expireNumber = 1;
+				}
+			},
             currentBucket() {
                 let pagination = this.pagination;
                 let current = pagination.current ? pagination.current : 1;
@@ -474,19 +492,60 @@
                 this.addrForm = {
                     signUrl: null,
                     openId: null,
+					isPublic:record.isPublic,
                     fileName: record.fileName,
                     resourceId: record.id,
-                    expireSeconds: 3600,
+                    expireSeconds: 7200,
+                    expireNumber: 2,
+                    expireUnit: 'hour',
                     bucketName: this.currentBucket
                 }
                 this.addrVisible = true;
                 this.$http.get('/member/access/' + this.currentBucket).then(response => {
                     this.openIds = response.data.result;
                     this.addrForm.openId = this.openIds[0].openId;
+                    if(record.isPublic){
+                        this.genAddr();
+                    }
                 });
             },
+            onExpireUnitChange(unit) {
+                if (unit === 'ever') {
+                    this.addrForm.expireNumber = 1;
+                }
+                this.addrForm.signUrl = '';
+            },
             genAddr() {
+                this.addrForm.signUrl = '';
                 let body = {...this.addrForm};
+                let x = 1;
+                let expireNumber = parseInt(this.addrForm.expireNumber);
+                if (expireNumber < 1) {
+                    this.$message.warn("时间不能小于1");
+                    return;
+                }
+                switch (this.addrForm.expireUnit) {
+                    case "minute":
+                        x = 60;
+                        break
+                    case "hour":
+                        x = 3600;
+                        break;
+                    case "day":
+                        x = 3600 * 24;
+                        break;
+                    case "month":
+                        x = 3600 * 24 * 30;
+                        break;
+                    case "year":
+                        x = 3600 * 24 * 30 * 365;
+                        break;
+                    case "ever":
+                        x = -1;
+                        expireNumber = 1;
+                        break;
+                }
+                body.expireSeconds = x * expireNumber;
                 this.$http.post('/member/access/sign', body).then(response => {
                     this.addrForm.signUrl = response.data.result;
                 });
